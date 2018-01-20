7b40665d9b0cb2edccc76b15c7a4a7de0345eac8
Robbie McKinstry <robbie@hashicorp.com>
2018-01-19 19:25:05 -0500

    FIX THIS DAMN PATH ISSUE

--- command/serve.go
+++ command/serve.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"net/http"
+	"net/url"
 
 	"github.com/urfave/cli"
 )
@@ -16,9 +17,20 @@ func Serve(c *cli.Context) error {
 		return errTooFewArgs
 	}
 	// fileserver := http.StripPrefix("/dist/", http.FileServer(assetFS()))
-	fileserver := http.FileServer(assetFS())
+	fileserver := injectDist(http.FileServer(assetFS()))
 	fmt.Println("grammar file: ", c.Args().First())
 	fmt.Println("http://localhost:8080/")
 	err := http.ListenAndServe(":8080", fileserver)
 	return err
+}
+
+func injectDist(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		r2 := new(http.Request)
+		*r2 = *r
+		r2.URL = new(url.URL)
+		*r2.URL = *r.URL
+		r2.URL.Path = "dist/" + r2.URL.Path
+		h.ServeHTTP(w, r2)
+	})
 }
