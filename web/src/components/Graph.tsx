35c204649eff141bd4f99ddafb61ea820e5caef2
Robbie McKinstry <robbie@hashicorp.com>
2018-01-22 20:09:20 -0500

    build graph to fill window

--- web/src/components/Graph.tsx
+++ web/src/components/Graph.tsx
@@ -2,20 +2,6 @@ import * as React from 'react';
 import * as GraphLib from 'react-d3-graph';
 import * as $ from 'jquery';
 
-let config = {
-    height: 600,
-    width: 1424,
-    nodeHighlightBehavior: true,
-    node: {
-        color: 'lightgreen',
-        size: 120,
-        highlightStrokeColor: 'blue'
-    },
-    link: {
-        highlightColor: 'lightblue'
-    }
-};
-
 class GrammarElem {
     name: string
     level: number
@@ -30,6 +16,7 @@ class GrammarElem {
 interface Props {}
 class Node {
     id: string
+    y: number
 
     constructor(id: string) {
         this.id = id;
@@ -45,12 +32,38 @@ class Edge {
     }
 }
 
+class Config {
+    height: number
+    width: number
+    nodeHighlightBehavior: boolean
+    node: Object
+    link: Object
+    staticGraph: boolean
+}
+
 export default class Graph extends React.Component {
-    
+    static initConfig(): Config {
+        return {
+            height: 600,
+            width: 1424,
+            nodeHighlightBehavior: true,
+            node: {
+                color: 'lightgreen',
+                size: 120,
+                highlightStrokeColor: 'blue'
+            },
+            link: {
+                highlightColor: 'lightblue'
+            },
+            staticGraph: false
+        };
+    }
+   
     state = {
         nodes: new Array<Node>(),
         links: new Array<Edge>(),
         root: "",
+        config: Graph.initConfig(),
     }
     constructor(props: Props) {
         super(props);
@@ -64,6 +77,11 @@ export default class Graph extends React.Component {
             new Edge("Sally", "Alice"),
             new Edge("Alice", "Harry"),
         ];
+        this.state.config = Graph.initConfig();
+    }
+
+    grammarMapper(elemRaw: any): GrammarElem {
+        return new GrammarElem(elemRaw.Name, elemRaw.Level, elemRaw.Children);
     }
 
     grammarCallback = (data: any) => {
@@ -71,21 +89,45 @@ export default class Graph extends React.Component {
         const grammar = data.grammar;
         var nodes = new Array<Node>();
         var edges = new Array<Edge>();
-        grammar.forEach((elemRaw: any) => {
-            const elem = new GrammarElem(elemRaw.Name, elemRaw.Level, elemRaw.Children);
-            nodes.push(new Node(elem.name));
-            var children: Array<string> = elem.children;
-            if (children === null) {
+        const grammarElems = grammar.map(this.grammarMapper);
+        grammarElems.forEach((elem : any) => { nodes.push(new Node(elem.name)); });
+        grammarElems.forEach((elem : GrammarElem) => {
+            if (elem.children === null) {
                 return;
             }
-            children.forEach((name: string) => {
+            elem.children.forEach((name: string) => {
                 edges.push(new Edge(elem.name, name));
             });
         });
+        /*
+        var yPos = 10;
+        nodes.map((n) => {
+            n.y = yPos;
+            yPos += 30;
+        });
+        */
+
         this.state.nodes = nodes;
         this.state.links = edges;
         this.state.root = root;
         this.setState(this.state);
+    }
+
+    componentDidMount() {
+        this.updateWindowDimensions();
+    }
+
+    updateWindowDimensions = () => {
+        const width:  number     = window.innerWidth;
+        const height: number     = window.innerHeight;
+        this.state.config.width  = width;
+        this.state.config.height = height;
+        this.setState(this.state);
+        window.addEventListener('resize', this.updateWindowDimensions);
+    }
+    
+    componentWillUnmount() {
+          window.removeEventListener('resize', this.updateWindowDimensions);
     }
 
     componentWillMount() {
@@ -98,7 +140,7 @@ export default class Graph extends React.Component {
             <GraphLib.Graph
                 id='graph-id'
                 data={this.state}
-                config={config}
+                config={this.state.config}
             />
         );
     }
