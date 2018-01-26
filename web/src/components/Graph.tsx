106f77146b4481096cf3768bebb69438e846966e
Robbie McKinstry <thesnowmancometh@gmail.com>
2018-01-26 00:25:09 -0500

    Positioning (#11)
    * build graph to fill window
    
    * Update graph

--- web/src/components/Graph.tsx
+++ web/src/components/Graph.tsx
@@ -2,20 +2,6 @@ import * as React from 'react';
 import * as GraphLib from 'react-d3-graph';
 import * as $ from 'jquery';
 
-let config = {
-    height: 600,
-    width: 1424,
-    nodeHighlightBehavior: true,
-    node: {
-        color: 'lightgreen',
-        size: 120,
-        highlightStrokeColor: 'blue'
-    },
-    link: {
-        highlightColor: 'lightblue'
-    }
-};
-
 class GrammarElem {
     name: string
     level: number
@@ -30,9 +16,13 @@ class GrammarElem {
 interface Props {}
 class Node {
     id: string
+    x: number
+    y: number
+    level: number
 
-    constructor(id: string) {
+    constructor(id: string, level: number) {
         this.id = id;
+        this.level = level;
     }
 }
 class Edge {
@@ -45,25 +35,56 @@ class Edge {
     }
 }
 
+class Config {
+    height: number
+    width: number
+    nodeHighlightBehavior: boolean
+    node: Object
+    link: Object
+    staticGraph: boolean
+}
+
 export default class Graph extends React.Component {
-    
+    static initConfig(): Config {
+        return {
+            height: 600,
+            width: 1424,
+            nodeHighlightBehavior: true,
+            node: {
+                color: 'lightgreen',
+                size: 120,
+                highlightStrokeColor: 'blue'
+            },
+            link: {
+                highlightColor: 'lightblue'
+            },
+            staticGraph: true
+        };
+    }
+   
     state = {
         nodes: new Array<Node>(),
         links: new Array<Edge>(),
         root: "",
+        config: Graph.initConfig(),
     }
     constructor(props: Props) {
         super(props);
         this.state.nodes = [
-            new Node("Harry"),
-            new Node("Sally"),
-            new Node("Alice"),
+            new Node("Harry", 1),
+            new Node("Sally", 1),
+            new Node("Alice", 1),
         ];
         this.state.links = [
             new Edge("Harry", "Sally"),
             new Edge("Sally", "Alice"),
             new Edge("Alice", "Harry"),
         ];
+        this.state.config = Graph.initConfig();
+    }
+
+    grammarMapper(elemRaw: any): GrammarElem {
+        return new GrammarElem(elemRaw.Name, elemRaw.Level, elemRaw.Children);
     }
 
     grammarCallback = (data: any) => {
@@ -71,21 +92,61 @@ export default class Graph extends React.Component {
         const grammar = data.grammar;
         var nodes = new Array<Node>();
         var edges = new Array<Edge>();
-        grammar.forEach((elemRaw: any) => {
-            const elem = new GrammarElem(elemRaw.Name, elemRaw.Level, elemRaw.Children);
-            nodes.push(new Node(elem.name));
-            var children: Array<string> = elem.children;
-            if (children === null) {
+        const grammarElems = grammar.map(this.grammarMapper);
+        grammarElems.forEach((elem : any) => { nodes.push(new Node(elem.name, elem.level)); });
+        grammarElems.forEach((elem : GrammarElem) => {
+            if (elem.children === null) {
                 return;
             }
-            children.forEach((name: string) => {
+            elem.children.forEach((name: string) => {
                 edges.push(new Edge(elem.name, name));
             });
         });
+    
+        let levelCounter = new Map<number, number>();
+
+        nodes.map((n) => {
+            if(levelCounter.has(n.level)) {
+                const curr = levelCounter.get(n.level);
+                levelCounter.set(n.level, curr+1);
+                n.x = 100*curr;
+            } else {
+                n.x= 100;
+                levelCounter.set(n.level, 1);
+            }
+            n.y = 30*n.level;
+        });
+
         this.state.nodes = nodes;
         this.state.links = edges;
         this.state.root = root;
         this.setState(this.state);
+    }
+
+    componentDidMount() {
+        this.updateWindowDimensions();
+        // this.state.config.staticGraph = true;
+        // this.setState(this.state);
+
+    }
+
+    updateWindowDimensions = () => {
+        const width:  number     = window.innerWidth;
+        const height: number     = window.innerHeight;
+        this.state.config.width  = width;
+        this.state.config.height = height;
+        this.setState(this.state);
+        window.addEventListener('resize', this.updateWindowDimensions);
+        window.addEventListener('keypress', this.snap);
+    }
+
+    snap = () => {
+        this.state.config.staticGraph = true;
+        this.setState(this.state);
+    }
+    
+    componentWillUnmount() {
+          window.removeEventListener('resize', this.updateWindowDimensions);
     }
 
     componentWillMount() {
@@ -94,11 +155,18 @@ export default class Graph extends React.Component {
     }
 
     render() {
+        if (this.state.root === "") {
+            return (
+                <p>Rendering…</p>
+            );
+        }
         return (
             <GraphLib.Graph
                 id='graph-id'
                 data={this.state}
-                config={config}
+                config={this.state.config}
+                // @ts-ignore: Property 'resetNodesPositions' does not exist on type 'Graph'.
+                onClickNode={this.resetNodesPositions}
             />
         );
     }
